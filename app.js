ae0b9f1efd031a286529f1ab24c32155c5551864
DanielRGilles <danryangilles@gmail.com>
2021-08-17 13:39:10 -0700

    added basic styling

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions and grab DOM elements
-
+const inputEl = document.getElementById('user-input');
+const buttonEl = document.getElementById('button');
+const resetEl = document.getElementById('reset');
+const resultEl = document.getElementById('recap-box');
+console.log(inputEl);
+console.log(buttonEl);
+console.log(resetEl);
+console.log(resultEl);
 // initialize global state
-
+let turnsLeft = 4;
 // set event listeners 
+buttonEl.addEventListener('click', () => {
+
+console.log('you clicked')
+  
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
